1a65f6ff30754401ba179d7fc4f3926ece15a666
Hunor Holzinger <hunorholzinger2@gmail.com>
2025-07-09 20:17:29 +0200

    Added nanosvg for scaling images

--- Gambling/Gambling.cpp
+++ Gambling/Gambling.cpp
@@ -1,7 +1,16 @@
 #include <SDL3/SDL.h>
+#define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #include <iostream>
 #include "Header.h"
+
+//nanosvg for converting number.svg to sizable images
+#include <stdio.h>
+#include <string>
+#include <math.h>
+#define NANOSVG_IMPLEMENTATION
+#include "nanosvg.h"
+
 using namespace std;
 
 int main(int argc, char* argv[]) {
@@ -10,6 +19,14 @@ int main(int argc, char* argv[]) {
     HWND consoleWindow = GetConsoleWindow();
     SDL_Window* window = SDL_CreateWindow("SDL3", 1000, 1000,  0);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+
+
+
+
+
+
+
+
     SetForegroundWindow(consoleWindow);
     cout << "Hello!\n";
     
